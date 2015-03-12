ce6e53e8783d3a901b089d8a0bb2ad62e1fdeae3
gabekoo <gabekoo@gmail.com>
2015-03-11 19:43:44 -0700

    Remove name validations because they were messing with sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,4 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :first_name, :presence => true
-  validates :last_name, :presence => true
 end
