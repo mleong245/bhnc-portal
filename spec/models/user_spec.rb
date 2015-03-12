2d71a1e2a7b94e8a3c1d8fcc34b67765364e55fa
gabekoo <gabekoo@gmail.com>
2015-03-11 19:57:31 -0700

    Remove validation tests from RSpec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,12 +5,4 @@ describe User do
     user = User.new({:first_name => 'foo', :last_name => 'bar', :password => '12345678'})
     expect(user.save).to be_false
   end
-  it "should not allow a blank first name" do
-    user = User.new({:email => 'foo@gmail.com', :last_name => 'bar', :password => '12345678'})
-    expect(user.save).to be_false
-  end
-  it "should not allow a blank last name" do
-    user = User.new({:email => 'foo@gmail.com', :first_name => 'bar', :password => '12345678'})
-    expect(user.save).to be_false
-  end
 end
