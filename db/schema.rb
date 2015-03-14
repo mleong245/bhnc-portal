b745c9dd6c4e944d55dcb49b1193848137a5646f
JIN KIM <matt.b.miller@gmail.com>
2015-03-13 17:33:22 -0700

    added rails_admin

--- db/schema.rb
+++ db/schema.rb
@@ -21,14 +21,14 @@ ActiveRecord::Schema.define(version: 20150313203800) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "first_name"
     t.string   "last_name"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
