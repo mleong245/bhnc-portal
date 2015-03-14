d2d3724f72f772cd0578e0f0e5e8a20570b5faa9
Kyle Hotchkiss <khotchkiss13@berkeley.edu>
2015-03-13 19:53:55 -0700

    Cleaned up our step defs
    I deleted the web steps file from the repo, as well as created the
    necessary steps that were used from it. I combined similar definition
    so that the amount of similar code was less.

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -1,5 +1,10 @@
+require 'uri'
+require 'cgi'
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "selectors"))
 
 #Populating the database
+
 Given /the following users exist/ do |users_table|
   users_table.hashes.each do |user|
     # each returned element will be a hash whose key is the table header.
@@ -24,4 +29,38 @@ Then /I give my information as the following: (.*)/ do |info|
   fill_in("user_email", :with => params[2])
   fill_in("user_password", :with => params[3])
   fill_in("user_password_confirmation", :with => params[4])
+end
+
+#Necessary Web Steps
+
+Given /^(?:|I )(should be|am?) on (.+)$/ do |should, page_name|
+  if should == "am"
+    visit path_to(page_name) if should == "am"
+  else
+    current_path = URI.parse(current_url).path
+    if current_path.respond_to? :should
+      current_path.should == path_to(page_name)
+    else
+      assert_equal path_to(page_name), current_path
+    end
+  end
+end
+
+When /^(?:|I )(press|follow?) "([^"]*)"$/ do |press, button|
+  click_button(button) if press == "press"
+  click_link(button) if press == "follow"
+end
+
+When /^(?:|I )fill in "([^"]*)" with "([^"]*)"$/ do |field, value|
+  fill_in(field, :with => value)
+end
+
+Then /^(?:|I )should( not)? see "([^"]*)"$/ do |neg, text|
+  if page.respond_to? :should
+    page.should have_content(text) unless neg
+    page.should have_no_content(text) if neg
+  else
+    assert page.has_content?(text) unless neg
+    assert page.have_no_content(text) if neg
+  end
 end
