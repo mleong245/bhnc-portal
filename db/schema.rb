8c1b532e538dee668c578a0e77172dcbd6964472
Kyle Hotchkiss <khotchkiss13@berkeley.edu>
2015-03-13 18:01:47 -0700

    Fixed bug of wrong link

--- db/schema.rb
+++ db/schema.rb
@@ -21,22 +21,22 @@ ActiveRecord::Schema.define(version: 20150313215314) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                            default: "",    null: false
+    t.string   "encrypted_password",               default: "",    null: false
     t.string   "first_name"
     t.string   "last_name"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                    default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "admin",                  default: false, null: false
-    t.boolean  "volunteer",              default: false, null: false
+    t.boolean  "admin",                            default: false, null: false
+    t.boolean  "volunteer",                        default: false, null: false
     t.string   "street_address"
     t.string   "city"
     t.integer  "zip_code"
