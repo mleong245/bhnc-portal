4c56ec069ab3e1c08e336056d6e9d4e2faa11973
gabekoo <gabekoo@gmail.com>
2015-03-31 21:30:15 -0700

    Allow custom fields to be updated. Go figure, I wrote really bad tests that didn't catch this.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :authenticate_user!
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+  
   def after_sign_out_path_for(resource_or_scope)
     user_session_path #root_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :email, :password) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password) }
   end
 end
