fa5ec3a2de5c41a9e8cd63cc3d4769a4f00fad72
Jin Kim <jinhunkim1@gmail.com>
2015-04-03 16:38:30 -0700

    new folder column name chagne

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe UserController do
   before do
     @user = User.create(:first_name => "First", :last_name => "Last", :email => "email@email.com", :password => "12345678")
-    @event = Event.create({:name => 'bar', :location => 'foo', :start => 'March 31st 2015 8:00', :end => 'March 31st 2015 9:00'})
+    @event = Event.create({:name => 'bar', :location => 'foo', :starts_at => 'March 31st 2015 8:00', :end => 'March 31st 2015 9:00'})
     sign_in @user
     Timecop.freeze DateTime.parse("March 30th 2015")
   end
