967789103e5e546fa30218f2650cc0bd0ce7562c
gabekoo <gabekoo@gmail.com>
2015-04-12 01:07:16 -0700

    Separate requests by location

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,11 @@
 class UserController < ApplicationController
   def show
     @events = Event.upcoming
-    @space_rental_requests = SpaceRentalRequest.all
     @space_rental_locations = SpaceRentalRequest.allLocations
+    @space_rental_requests = {}
+    @space_rental_locations.each do |location|
+        @space_rental_requests[location] = SpaceRentalRequest.where(:location => location)
+    end
   end
 
   def volunteer
