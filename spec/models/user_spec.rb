20b7e6297a76bb1199e8f8dbfadadf60b17c0eac
Kyle Hotchkiss <khotchkiss13@berkeley.edu>
2015-04-17 20:42:40 -0700

    Added User and Available Hour model rspec tests
    Added the rspec tests for the User and Available Hours model to improv
    test coverage

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,17 @@ describe User do
     user = User.new({:first_name => 'foo', :last_name => 'bar', :password => '12345678'})
     expect(user.save).to be_false
   end
+
+  it "says if a user is available" do
+    @user = User.create(:first_name => "First", :last_name => "Last", :email => "email@email.com", :password => "12345678")
+    @hour = AvailableHour.create(:day_of_week => "Sunday", :start => "8:00AM", :end => "8:30AM")
+    @user.available_hours << @hour
+    expect(@user.available?("Sunday", "8:00AM")).to be_true
+  end
+
+  it "says if a user is not available" do
+    @user = User.create(:first_name => "First", :last_name => "Last", :email => "email@email.com", :password => "12345678")
+    @hour = AvailableHour.create(:day_of_week => "Sunday", :start => "8:00AM", :end => "8:30AM")
+    expect(@user.available?("Sunday", "8:00AM")).to be_false
+  end
 end
